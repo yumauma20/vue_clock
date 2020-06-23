67c3c457cadf7d9b8507e691870ae3866c31b9df
yumauma20 <71yuma10@gmail.com>
2020-06-23 14:19:12 +0900

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
-
+  <div>
+    <Clock class="clock" location="TOKYO" :diff="0"/>
+    <Clock class="clock" location="NY" :diff="-14"/>
+    <Clock class="clock" location="LA" :diff="-17"/>
+    <Clock class="clock" location="UK" :diff="-9"/>
+    <Clock class="clock" location="BRAZIL" :diff="-12"/>
+    <Clock class="clock" location="SYDNEY" :diff="1"/>
+    <Clock class="clock" location="DUBAI" :diff="-5"/>
+  </div>
 </template>
 
 <script>
+import Clock from "@/components/Clock"
 
+export default {
+  components: {
+    Clock,
+  },
+}
 </script>
 
-<style>
+<style scoped>
+.clock {
+  width: 90%;
+  max-width: 600px;
+  margin: 30px auto;
+}
+</style>
 
+<style>
+html {
+  font-size: 62.5%;
+}
+ 
+body {
+  margin: 0;
+}
+ 
+p {
+  margin: 0;
+}
 </style>
